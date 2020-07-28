7d22d7101a185a4d153b9000f0e0df04e5ef9199
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-07-28 17:58:45 +0200

    js/custom.js: creation

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+
+var colors = [
+    "red",
+    "green",
+    "blue",
+    "black",
+    "orange"
+]
+
+var i= 0;
+for (i = 0; i < document.getElementsByClassName("box-title").length; i++) {
+    document.getElementsByClassName("box-title")[i] = "red";
+}
+
+var list = document.getElementsByClassName("box-title");
+
+var x = Math.floor(Math.random() * list.length);
+
+for (var i = 0; i < list.length; i++) {
+    list[i].style.background = colors[x];
+}
+
+Document.getElementsByClassName("box-title")[0].style.background = "red";
+
